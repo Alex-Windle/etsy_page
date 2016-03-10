3200868b27f2b203bf3ae36dca2adaa626b17b9c
Alex Windle <alexwindle85@gmail.com>
2016-03-10 14:44:24 -0500

    add products listing

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
 // Javascript Entry Point
+import data from './items';
+import $ from 'jquery';
+
+var items = data.results; //array
+
+function createListing(product) {
+	
+	var picture = product.Images[0].url_170x135;
+
+	return `
+	<div class="listing">
+		<div class="picture"><img src = "${picture}" alt="photo"></div>
+		<div class="title">${product.title}</div>
+		<div class="price">${product.price}</div>
+	</div>
+	`; 
+};
+
+items.forEach(function(product){
+	$('main').append(createListing(product));
+});
+
